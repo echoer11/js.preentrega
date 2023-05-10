d1878ce510249344a016888e6127bce6ed44b59a
Bruno Raffaelli <92704506+echoer11@users.noreply.github.com>
2023-05-10 17:51:11 -0300

    primera

---
+++ app.js
@@ -0,0 +1,78 @@
+/* let comida = prompt("que queres comer?");
+
+switch (comida) {
+  case "pizza":
+    console.log(`Pedido ${comida}, valor $100`);
+    break;
+
+  case "empanadita":
+    console.log(`Pedido ${comida}, valor $1000`);
+    break;
+
+  case "milanesa":
+    console.log(`Pedido ${comida}, valor $1000`);
+    break;
+  case "arroz":
+    console.log(`Pedido ${comida}, valor $200`);
+    break;
+}
+function name(params) {
+    
+}
+gfgfgdf() */
+
+
+// Funcion seleccionar nivel de membresía:
+
+function membership(){
+let customerStatus = prompt("Please type your membership status:\nBronze \nSilver \nGold");
+let discount = 0;
+if (customerStatus === "Gold") {
+  discount = 20;
+} else if (customerStatus === "Silver") {
+  discount = 10;
+} else if (customerStatus === "Bronze") {
+  discount = 5;
+} else {
+  console.log("You are not a member.");
+}
+console.log("Your discount is " + discount + "% off!");
+let finalResult = 100 - discount;
+return finalResult;
+}
+
+let casioGshock = 80000;
+let seikoProspex = 110000;
+let rolexSubmariner = 950000;
+
+let discountApplied = membership();
+
+// Funcion para elegir producto y aplicar descuento
+
+function addingProduct() {
+    let price = 0;
+    let chooseProduct;
+  
+    while (true) {
+      chooseProduct = parseInt(prompt("Which watch would you like? \n1. Casio G-Shock \n2. Seiko Prospex \n3. Rolex Submariner"));
+      
+      if (chooseProduct === 1) {
+        price = discountApplied * casioGshock / 100;
+        break;
+      } else if (chooseProduct === 2) {
+        price = discountApplied * seikoProspex / 100;
+        break;
+      } else if (chooseProduct === 3) {
+        price = discountApplied * rolexSubmariner /100;
+        break;
+      } else {
+        alert("Choose a valid product");
+      }
+    }
+    
+    alert("The price for your chosen product is " + price + ".-");
+  }
+
+addingProduct();
+
+
